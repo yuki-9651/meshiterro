a3cb73bc74678f6c9b6ba402857aad202667e006
yuki-9651 <165871172+yuki-9651@users.noreply.github.com>
2024-06-03 08:20:58 +0000

    [Finish]6-8

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,5 @@
 class HomesController < ApplicationController
-  def top
+  def about
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
-  get 'homes/top'
+  devise_for :users
   root to: "homes#top"
+  get 'homes/about' => "homes#about", as: 'homes_about'
 end
